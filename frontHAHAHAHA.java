c10dd8ae856655dfcabe41551f2acb83c3d63ef1
Francisco Aureliano Teixeira Assis Simões <francisco.aureliano00k7@gmail.com>
2025-08-01 16:09:51 -0300

    Add files via upload

---
+++ frontHAHAHAHA.java
@@ -0,0 +1,12 @@
+
+public class frontHAHAHAHA {
+
+	import javax.swing.*;
+
+	import java.awt.*;
+	public static void main(String[] args) {
+    JFrame HAHAHA = new JFrame("tela do kugutsu");
+
+	}
+
+}}
